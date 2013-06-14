749d5585b2029ed55d698f9d358a4a7cd7497ff2
eucacolby <colby.dyess@eucalyptus.com>
2013-06-13 20:44:24 -0400

    Updated GA code insertion
    * Improved script for inserting Google Analytic code. Better error
    checking. Fixed bugs.
    * Added file containing GA snippet

--- reporting/add-google-analytics.sh
+++ reporting/add-google-analytics.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]
+GOOGLE_SITE_ID="UA-8504735-6"
+
+#############################################################
+#
+# Basic error checking of the parameters
+#
+
+if [ $# -ne 2 ]
   then
     echo "Usage: add-google-analytics [filename] [directory]
 
@@ -8,26 +15,63 @@ if [ $# -eq 0 ]
     directory - location of html files
 
     "
+    exit 1
 fi
 
-#i############################################################
+if [ ! -e $1 ]
+   then
+   echo "$1 does not exist or is invalid"
+   echo
+   exit 1
+fi
+
+if [ ! -d $2 ]
+   then
+   echo "$2 is not a directory"
+   echo
+   exit 1
+fi
+
+
+#############################################################
+#
+# Google Analytic site ID is unique to each site
+# Be sure this matches the value in your GA snippet!
+#
+grep -iql "$GOOGLE_SITE_ID" $1
+if [ $? != "0" ]
+   then
+   echo "Google Site ID mistach. Expected: GOOGLE_SITE_ID"
+   echo "Make sure the ID in $(basename $0) and $1 match"
+   echo
+   exit 1
+fi
+
+
+#############################################################
 #
 # Looks for a .html files in the specified directory and
 # injects the google analytic code in the given file. The
 # script runs a simple check to see if the code has already
 # been injected - otherwise the .html file gets a bit messy.
-#
+# Also many html files are just html snippets. It's critical
+# that we don't insert GA code into those, or who knows
+# what will break.
 #
 for filename in `find $2 -name *.html`
 do
-  grep -iql "UA-8504735-1" $filename
-  if [ $? != "0" ]
-    then
-      sed -e '/<\body>/,$d' < $filename > temp2.txt
-      cat $1 >> temp2.txt
-      grep '</body>' $filename | head -1 >> temp2.txt
-      sed -e '1,/<\/body>/d' < $filename >> temp2.txt
-      mv temp2.txt $filename
-      echo "Modified: $filename"
+  grep -iql "</body>" $filename
+  if [ $? = "0" ]
+     then
+     grep -iql "$GOOGLE_SITE_ID" $filename
+     if [ $? != "0" ]
+        then
+        sed -e '/<\/body>/,$d' < $filename > temp2.txt
+        cat $1 >> temp2.txt
+        grep '</body>' $filename | head -1 >> temp2.txt
+        sed -e '1,/<\/body>/d' < $filename >> temp2.txt
+        mv temp2.txt $filename
+        echo "Modified: $filename"
+    fi
   fi
 done
